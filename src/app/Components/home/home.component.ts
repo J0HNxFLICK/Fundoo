5db0ac6e4ddb04619ad7c864d8da348198286660
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2023-03-22 20:49:02 +0530

    Complete toolbar revamp

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnDestroy {
   fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
 
   fillerContent = Array.from(
-    {length: 50},
+    {length: 4},
     () =>
       `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
